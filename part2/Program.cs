17471c7e1dda489e45aa8f30c73e2d67cd9e4384
Alex <alex-txf1@mail.ru>
2022-06-27 10:08:32 +0300

    решение второй задачи

---
+++ part2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("введите число    ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число    ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число    ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+if(number1 > num)
+{
+    num = number1;
+}
+if(number2 > num)
+{
+    num = number2;
+}
+if(number3 > num)
+{
+    num = number3;
+}
+Console.Write(num);
