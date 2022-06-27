bea5a24290fa392c34e38611bd6053990bad7662
Alex <alex-txf1@mail.ru>
2022-06-27 11:03:22 +0300

    решение четвертой задачи

---
+++ part4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("введите число    ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+for (int x = 2; x < number1; x = x + 1)       
+if (x%2==0)            
+Console.WriteLine(x);
