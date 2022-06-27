9f15ed5810350b752f51bfc0da8cf6739070a1c3
Alex <alex-txf1@mail.ru>
2022-06-27 09:42:23 +0300

    внесено решение первой задачи

--- part1/Program.cs
+++ part1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("введите число    ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число    ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.Write("Большее число  ");
+    Console.Write(number1);
+}
+else
+{
+    Console.Write("Большее число   ");
+    Console.Write(number2);
+}
+
